e76cb7e7d3aeb78c87a4bcd793c95b76cb944501
KimBOO <qhdud0110@gmail.com>
2024-08-18 22:11:37 +0900

    초기세팅

---
+++ src/main/java/com/KidLove/KidLoveApplication.java
@@ -0,0 +1,13 @@
+package com.KidLove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidLoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KidLove/KidLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KidLove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidLoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
